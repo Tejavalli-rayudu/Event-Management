812fc735f497332bdd8173f73ab8698067e817b2
Tejavalli-rayudu <tejavallirayudu@gmail.com>
2026-08-04 14:25:39 +0530

    Student Dashboard

--- frontend/src/Pages/StudentDashboard.jsx
+++ frontend/src/Pages/StudentDashboard.jsx
@@ -1,46 +1,11 @@
-import React from "react";
+function StudentDashboard(){
 
-const StudentDashboard = () => {
-  return (
-    <div style={{ padding: "20px" }}>
+  return(
+    <div>
       <h1>Student Dashboard</h1>
-
-      <p>Welcome Student 👨‍🎓</p>
-
-      <h3>Available Events</h3>
-
-      <table border="1" cellPadding="10">
-        <thead>
-          <tr>
-            <th>Event Name</th>
-            <th>Date</th>
-            <th>Location</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>Tech Fest</td>
-            <td>2026-08-15</td>
-            <td>Auditorium</td>
-            <td>
-              <button>Register</button>
-            </td>
-          </tr>
-
-          <tr>
-            <td>Coding Contest</td>
-            <td>2026-08-20</td>
-            <td>Lab Block</td>
-            <td>
-              <button>Register</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
-  );
-};
+  )
+
+}
 
 export default StudentDashboard;
